91aa212f1e1cf99fd28e65ac397d2e0a238612fc
Gary Chewing <garychu97@gmail.com>
2024-08-13 11:25:19 +0800

    [Gary] feat: initialise spring

---
+++ src/main/java/com/chewingk/pitter/PitterApplication.java
@@ -0,0 +1,13 @@
+package com.chewingk.pitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chewingk/pitter/PitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chewingk.pitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
